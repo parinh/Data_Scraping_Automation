7bd9e7df1b28ca85640597b85e633f9859609df0
parin <hopper.ken42@gmail.com>
2021-08-11 17:35:06 +0700

    add try-catch in thaijo funcc

--- thaijo.py
+++ thaijo.py
@@ -23,66 +23,74 @@ class Thaijo:
         _authors_affiliation = "no affiliation"
         _issue_id = "no id"
 
+        try:
+            abstract_clean = response.json().get("result")[0].get("abstract_clean").get('th_TH')
+            if(abstract_clean):
+                _abstract_clean = abstract_clean
         
-        abstract_clean = response.json().get("result")[0].get("abstract_clean").get('th_TH')
-        if(abstract_clean):
-            _abstract_clean = abstract_clean
-       
-        title =response.json().get("result")[0].get("title").get('th_TH')
-        if(title):
-            _title=title
-      
-        article_url =response.json().get("result")[0].get("articleUrl")
-        if(article_url):
-            _article_url = article_url
-       
-
-        issue_date_published=response.json().get("result")[0].get("issueDatePublished")
-        if(issue_date_published):
-            _issue_date_published=issue_date_published
-       
-        issue_cover_image=response.json().get("result")[0].get("issueCoverImage").get("en_US")
-        if(issue_cover_image):
-            _issue_cover_image=issue_cover_image
+            title =response.json().get("result")[0].get("title").get('th_TH')
+            if(title):
+                _title=title
         
-        authors_full_name = response.json().get("result")[0].get("authors")[0].get("full_name").get("th_TH")
-        if(authors_full_name):
-            _authors_full_name = authors_full_name
-        
-        authors_affiliation = response.json().get("result")[0].get("authors")[0].get("affiliation").get("th_TH")
-        if(authors_affiliation):
-            _authors_affiliation = authors_affiliation
-        
-        issue_id=response.json().get("result")[0].get("issue_id")
-        if(issue_id):
-            _issue_id=issue_id
+            article_url =response.json().get("result")[0].get("articleUrl")
+            if(article_url):
+                _article_url = article_url
         
 
-        research = {
-            "issue_id":_issue_id,
-            "title": _title,
-            "abstract_clean":_abstract_clean,
-            "article_url":_article_url,
-            "issue_date_published":_issue_date_published,
-            "issue_cover_image":_issue_cover_image,
-            "authors_full_name":_authors_full_name,
-            "authors_affiliation":_authors_affiliation
-        }
+            issue_date_published=response.json().get("result")[0].get("issueDatePublished")
+            if(issue_date_published):
+                _issue_date_published=issue_date_published
+        
+            issue_cover_image=response.json().get("result")[0].get("issueCoverImage").get("en_US")
+            if(issue_cover_image):
+                _issue_cover_image=issue_cover_image
+            
+            authors_full_name = response.json().get("result")[0].get("authors")[0].get("full_name").get("th_TH")
+            if(authors_full_name):
+                _authors_full_name = authors_full_name
+            
+            authors_affiliation = response.json().get("result")[0].get("authors")[0].get("affiliation").get("th_TH")
+            if(authors_affiliation):
+                _authors_affiliation = authors_affiliation
+            
+            issue_id=response.json().get("result")[0].get("issue_id")
+            if(issue_id):
+                _issue_id=issue_id
+            
+
+            research = {
+                "issue_id":_issue_id,
+                "title": _title,
+                "abstract_clean":_abstract_clean,
+                "article_url":_article_url,
+                "issue_date_published":_issue_date_published,
+                "issue_cover_image":_issue_cover_image,
+                "authors_full_name":_authors_full_name,
+                "authors_affiliation":_authors_affiliation
+            }
+        
 
         # print(research)
 
-        return research
+            return research
+        except Exception as e:
+            print(e)
+            
 
         
 
     def getData (self,keyword,page):
-        query = {"term":keyword,"page":page,"size":1,"strict":True,"title":True,"author":True,"abstract":True}
-        response = requests.post('https://www.tci-thaijo.org/api/articles/search/', json=query)
-        result = response.json().get("result")[0]
-        # print(result)
-        # print("\n")
-        # self.getItem(response)
-        self.datas.append(self.getItem(response))
+        try:
+            query = {"term":keyword,"page":page,"size":1,"strict":True,"title":True,"author":True,"abstract":True}
+            response = requests.post('https://www.tci-thaijo.org/api/articles/search/', json=query)
+            result = response.json().get("result")[0]
+            # print(result)
+            # print("\n")
+            # self.getItem(response)
+            self.datas.append(self.getItem(response))
+            
+        except Exception as e:
+            print(e)
 
     def toCsv (self,datas):
         with open(config("FILE"), "w", encoding="utf-8", newline="") as csvfile:
